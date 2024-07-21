d99fa4d78058c75903184b0073f0fb5e77a1923f
isa <isaismail3000@gmail.com>
2024-07-21 20:08:22 +0300

    Create function that will fetch weather of city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+async function getWeather(loc) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=metric&key=SF29LX5N55MTZUTLHKL48R3R6`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
