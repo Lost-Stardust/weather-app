4f5b96ca24818b1596fc21a5af067e221d78eda6
isa <isaismail3000@gmail.com>
2024-08-06 15:25:07 +0300

    Import function and svg, run func

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./styles.css";
 import { assignIcons } from "./icons";
+import { assignData } from "./assign";
+import icon from "./assets/magnify.svg";
+assignData();
 assignIcons();
 // Maybe you can put function calls inside a async function here that could await the result of weather data/display of weather data and then while awaiting for that, it could display a loading icon.
