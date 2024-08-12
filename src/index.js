13881a2f326dc23cadb85f1b23f786039d91a518
isa <isaismail3000@gmail.com>
2024-08-12 15:21:54 +0300

    Import loading and loadingicon, run while waiting for response

--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import { assignData } from "./assign";
 import icon from "./assets/magnify.svg";
 import sunset from "./assets/weather-sunset-down.svg";
 import sunrise from "./assets/weather-sunset-up.svg";
+import loadingIcon from "./assets/loading.svg";
+import { loading } from "./loading";
 import { getWeatherData } from "./weather";
 const searchBar = document.querySelector("#search");
 const submit = document.querySelector('button[type="submit"]');
 // let dataa;
 submit.addEventListener("click", async (event) => {
   event.preventDefault();
+  loading(true);
+  // Run imported function that shows a loading icon
   const data = await getWeatherData(searchBar.value);
-  //   dataa = data;
   console.log(data);
+  // Run again to remove loading icon
+  loading(false);
   assignData(await data);
   assignIcons(await data);
 });
